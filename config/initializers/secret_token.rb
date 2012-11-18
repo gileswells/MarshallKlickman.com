7b80fe878b38c7168691afe0942d52c4b45ea48a
Marshsall Klickman <mklickman1@gmail.com>
2012-11-18 00:21:26 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+MarshallklickmanCom::Application.config.secret_token = 'ebaea04c47de2bb86253e79517f97a10fcd9e1790ac3b7a101e7745954497ddb2f96eb8ecaefa282b2d18550038481b5e31ca0d4444dee29e93e0e921b38d6d6'
